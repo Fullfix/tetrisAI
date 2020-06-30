b01e4ae27838e5a7ca27826c5a97bf2d1d0016b4
Nikita Breskanu <50447080+Fullfix@users.noreply.github.com>
2020-06-30 11:08:06 +0400

    create genetic

--- genetic.py
+++ genetic.py
@@ -2,9 +2,11 @@ import random
 import config
 import numpy as np
 import pickle
+import keras
 from deap.tools import cxTwoPoint, cxUniform, cxBlend, mutGaussian
 from copy import deepcopy, copy
 from Agent import Agent
+from Env import Env
 
 def eval_agent(agent: Agent):
     return agent.score + 0.05 * agent.totaltime
@@ -14,3 +16,29 @@ def mutate(weights: list):
         M = np.random.normal(config.MU, config.SIGMA, W.shape)
         P = np.random.choice(2, W.shape, p=[1-config.INDPB, config.INDPB])
         W += M * P
+
+def choose_best(population: list, n: int):
+    l = config.NUM_AGENTS
+    sorted_pop = sorted(population, eval_agent, reverse=True)
+    sorted_pop = [x.model.get_weights() for x in sorted_pop]
+    return sorted_pop[:n], sorted_pop[n:l-n], sorted_pop[l-n:]
+
+def get_avg_time(population: list):
+    return sum(list(map(lambda x: x.totaltime, population))) / len(population)
+
+def generate_next_pop(env: Env):
+    # choose best agents
+    population = env.get_population()
+    bestpopulaton, middle, worst = choose_best(population, config.BEST_IND_NUM)
+    # crossover middle
+    for i in range(0, len(middle), 2):
+        middle[i], middle[i+1] = cxBlend(middle[i], middle[i+1], settings.ALPHA)
+    newpopulation = bestpopulaton + middle + deepcopy(bestpopulaton)
+    # mutate
+    for weights in newpopulation:
+        mutate(weights)
+    # needed to optimize time
+    keras.backend.clear_session()
+    # set weights
+    env.change_weights(newpopulation)
+    env.reset()
